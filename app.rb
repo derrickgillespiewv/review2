a9a5ad390fe8d21d0252217dccc0a3facf415636
Derrick Gillespie <derrickgillespiewv@gmail.com>
2017-08-19 09:39:50 -0400

    App works, with simple post structure, trying to do get/post methods

--- app.rb
+++ app.rb
@@ -3,47 +3,62 @@ require_relative"clean.rb"
 
 enable :sessions
 
+ 
+# Working on version with get-post structure
+
+# get '/' d
+# 	erb :sandwich_parts
+# end
+
+# post '/sandwich_parts' do 
+
+# 	meat = params[:meat]
+# 	veg = params[:veg]
+# 	cheese = params[:cheese]
+# 	meat =clean(meat)
+# 	veg =clean(veg)
+# 	cheese =clean(cheese)
+# 	  erb :sandwich_parts, :locals => {:meat => meat, :cheese => cheese, :veg => veg}
+# 	redirect '/selection?meat=' + meat + '&veg=' + veg + '&cheese=' + cheese
+# end
+
+# get '/selection' do 
+# 	meat = params[:meat]
+# 	veg = params[:veg]
+# 	cheese = params[:cheese]
+# 	choice = params[:choice]
+# 	erb :selection, :locals => {:choice => choice}
+# end 
+
+# post '/selection' do 
+# 	meat = params[:meat]
+# 	veg = params[:veg]
+# 	cheese = params[:cheese]
+# 	choice = params[:choice]
+# 	redirect '/results?meat=' + meat + '&veg=' + veg + '&cheese=' + cheese + '&choice=' + choice
+# end 
+
+# get '/results' do 
+# 	choice = params[:choice]
+# 	choice.to_s
+# 	erb :results, :locals => {:choice => choice}
+# end 
+
+
+
+require "sinatra"
+
 get '/' do
   erb :sandwich_parts
 end
 
-get '/selection' do 
-erb :selection
+post '/parts' do
+  food = params[:food]
+  erb :selection, :locals=>{:food=>food}
 end
 
-post '/selection' do 
-
-meat = params[:meat]
-veg = params[:veg]
-cheese = params[:cheese]
-meat =clean(meat)
-veg =clean(veg)
-cheese =clean(cheese)
-choice = params[:choice]
-choice.to_s
-erb :selection, :locals => {:meat => meat, :cheese => cheese, :veg => veg}
-redirect '/results?choice=' + choice
- # + '&meat=' + meat + '&veg=' + veg + '&veg=' + veg + '&cheese=' + cheese
-end 
-
-get '/results' do 
-choice = params[:choice]
-choice.to_s
-erb :results, :locals => {:choice => choice}
-end 
-
-
-# erb :results, :locals => {:choice => choice}
-# end
-
-# post '/results' do
-# choice = params[:choice]
-# erb :results, :locals => {:choice => choice}
-# end
-
-
-# post '/results' do
-# choice = params[:choice]
-#  choice.to_s
-# redirect '/results?choice=' + choice 
-# end
+get '/results' do
+    food = params[:food]
+    toppings = params[:toppings]
+    erb :results, :locals=>{:food=>food, :toppings=>toppings}
+  end
